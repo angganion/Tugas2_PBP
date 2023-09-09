18b5406df06063a07dad189efc9aeed266c29874
Wahyu Ridho Anggoro <120027733+angganion@users.noreply.github.com>
2023-09-09 14:19:03 +0700

    typo html

--- main/views.py
+++ main/views.py
@@ -6,11 +6,11 @@ def show_main(request):
         'name1': 'orang1',
         'deskripsi1': 'baarang mahal',
         'total1' : 0,
-        'orang2' : 'orang2',
+        'name2' : 'orang2',
         'deskripsi2' : 'barang bagus',
         'total2' : 0,
-        'orang3' : 'orang3',
-        'deslripsi3' : 'barang murah',
+        'name3' : 'orang3',
+        'deskripsi3' : 'barang murah',
         'total3' : 0,
 
     }
